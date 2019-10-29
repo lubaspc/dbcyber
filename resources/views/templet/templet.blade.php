6c394c3e41ba81cd6735d1af75da7cd04f4fca9e
CharlyEsCaz <cyescaz3@gmail.com>
2019-10-29 09:30:51 -0600

    Navbar creation

--- resources/views/templet/templet.blade.php
+++ resources/views/templet/templet.blade.php
@@ -17,7 +17,7 @@
     @stack('css')
     <title>@yield('title')-Lubins</title>
 </head>
-<body class="p-0 m-0 w-100 h-100">
+<body class="p-0 m-0 w-100 h-100 bg-black">
     @yield('body')
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
